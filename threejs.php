69cf4db3a9a7f82906bc4839602f0efc1fe13c60
wetinx <31441910+wetinx@users.noreply.github.com>
2017-10-11 01:18:36 +0800

    2

--- threejs.php
+++ threejs.php
@@ -7,81 +7,16 @@
 	<title>jQuery EasyUI Demo</title>
 	<link rel="stylesheet" type="text/css" href="http://www.w3cschool.cc/try/jeasyui/themes/default/easyui.css">
 	<link rel="stylesheet" type="text/css" href="http://www.w3cschool.cc/try/jeasyui/themes/icon.css">
+   <link rel="stylesheet" type="text/css" href="mystyle.css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
 	<script type="text/javascript" src="http://www.w3cschool.cc/try/jeasyui/jquery.easyui.min.js"></script>
-	<style>
-		.panel-body{
-			background:#f0f0f0;
-		}
-		.panel-header{
-			background:#fff url('images/panel_header_bg.gif') no-repeat top right;margin-top: -3px;
-		}
-		.panel-tool-collapse{
-			background:url('images/arrow_up.gif') no-repeat 0px -3px;
-		}
-		.panel-tool-expand{
-			background:url('images/arrow_down.gif') no-repeat 0px -3px;
-		}
-	</style>
+	
 </head>
 <body>
-<div id="wrapper1" style="margin:0 auto;position:relative;width:1024px;height:auto;background:#7190E0">
-
+<div id="wrapper1" style="margin:0 auto;position:relative;width:1024px;height:100px;background:#7190E0">
 <ul id="menu" style="list-style-type:none;margin:0;padding:0;overflow:hidden;">
-
-<style>
-li.yangshi{float:left;}
-.yangshi a{
-display:block;
-width:158.62px;
-font-weight:bold;
-color:#FFFFFF;
-background-color:#bebebe;
-text-align:center;
-padding:6px;
-text-decoration:none;
-text-transform:uppercase; 
-font-size: 25px;
-font-weight: bold;
-	}
-.yangshi a:hover {
-	background-color:#000000;
-	}
-	.yangshi a:active
-{
-background-color:#cc0000;
-}
-li.yangshi1{display:true}
-.yangshi1 a{
-display:block;
-width:158.62px;
-font-weight:bold;
-color:#FFFFFF;
-background-color:#bebebe;
-text-align:center;
-padding:6px;
-text-decoration:none;
-text-transform:uppercase; 
-font-size: 25px;
-font-weight: bold;
-display:true
-	}
-.yangshi1 a:hover {
-	background-color:#000000;
-	}
-	.yangshi1 a:active 
-{
-background-color:#cc0000;
-}
-div.byside{width:200px;height:1024px;background:#7190e0;padding:5px;float:left;}
-div.easyui-panel{margin-top: 0px;}
-
-
-</style>
-
-
 <li class="yangshi"><a href="#threejs">three.js</a>
-    <ul id="menu1" style="list-style-type:none;margin:0;padding:0;>
+    <ul id="menu1" class="dropdown-conent" style="list-style-type:none;margin:0;padding:0;>
       <li class="yangshi1"><a href="#xcdv">Item 3-1</a></li>
       <li class="yangshi1"><a href="#xcv">Item 3-2</a></li>
       <li class="yangshi1"><a href="#drf">Item 3-3</a></li>
@@ -94,18 +29,13 @@ div.easyui-panel{margin-top: 0px;}
 <li class="yangshi"><a href="#news">News</a></li>
 <li class="yangshi"><a href="#contact">Contact</a></li>
 <li class="yangshi"><a href="#about">About</a></li>
-
-
 </ul>
 </div>
-
 
 <div id="wrapper2" style="margin:0 auto;position:relative;width:1024px;height:auto;background:#F00;">
 <div class="byside" >
     
-    
-    
-    <div class="easyui-panel" title="Manual 开发文档" collapsible="true" style="width:200px;    height:auto;padding:10px;">
+<div class="easyui-panel" title="Manual 开发文档" collapsible="true" style="width:200px;    height:auto;padding:10px;">
 			View as a slide show<br/>
 			Order prints online<br/>
             Order prints online
